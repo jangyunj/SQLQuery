49fa673d635058f6133e9f44a73dab16b530cef5
Yun Jang <jang.yunj7@gmail.com>
2023-03-21 23:45:08 -0400

    initial commit

---
+++ SQLQuery.sql
@@ -0,0 +1,16 @@
+CREATE TABLE demo (
+id INT 
+);
+
+create table demo(id int  j);
+
+
+INSERT INTO demo (id) VALUES (1);
+INSERT INTO demo (id) VALUES (2);
+INSERT INTO demo (id) VALUES (3);
+
+SELECT id
+FROM demo;
+
+CREATE TABLE error(
+id INT);
